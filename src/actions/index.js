740583a46fbc2b48baf06dbc890ef5379a99d4bc
Fred Baa <frederickbaa@gmail.com>
2017-10-04 17:32:37 +0800

    support data fetch and token handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { browserHistory } from 'react-router';
-import {AUTH_USER, UNAUTH_USER, AUTH_ERROR} from '../actions/types';
+import {AUTH_USER, UNAUTH_USER, AUTH_ERROR, FETCH_MESSAGE} from '../actions/types';
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -35,6 +35,20 @@ export function signupUser({ email, password }) {
           browserHistory.push("/feature");
         })
         .catch(error => dispatch(authError(error.response.data.error)))
+  }
+}
+
+export function fetchMessage() {
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token')}
+    })
+      .then(response => {
+        dispatch({
+          type: FETCH_MESSAGE,
+          payload: response.data.message
+        })
+      });
   }
 }
 
--- src/components/feature.js
+++ src/components/feature.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Feature extends Component {
+  componentWillMount() {
+    this.props.fetchMessage();
+  }
+
   render() {
     return (
       <div>
-        This is a feature.
+        Message from server:
+        <div>
+          { this.props.message }
+        </div>
       </div>
     );
   }
 }
 
-export default Feature;
+function mapStateToProps(state){
+  return { message: state.auth.message }
+}
+
+export default connect(mapStateToProps, actions)(Feature);
